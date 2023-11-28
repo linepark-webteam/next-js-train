094fb866aa4d3173e9b73737e13c8ede34fc3686
DESKTOP-2QJH1HL\19 <ko.nagai.0801@gmail.com>
2023-11-28 13:33:12 +0900

    First Commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+// pages/about.js
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page.</p>
+      <nav>
+        <a href="/">Home</a>
+      </nav>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js Site</h1>
+      <p>This is the home page.</p>
+      <nav>
+        <a href="/about">About Us</a>
+      </nav>
+    </div>
+  )
+}
